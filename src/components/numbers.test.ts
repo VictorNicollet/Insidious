0b5a82c0952b8dcc882459a6cde8995736e19f06
Victor Nicollet <victor@nicollet.net>
2021-08-13 12:28:09 +0200

    Fix display issue for X.999 fractional values

--- src/components/numbers.test.ts
+++ src/components/numbers.test.ts
@@ -8,3 +8,9 @@ test("integer(10)", () =>
 
 test("integer(1234)", () => 
     expect(N.integer(1234)).toBe("1 234"))
+
+test("decimal(2.2)", () => 
+    expect(N.decimal(0.4 + 3 * 0.6)).toBe("2.2"))
+
+test("signedDecimal(2.2)", () => 
+    expect(N.signedDecimal(2.2)).toBe("+2.2"))
--- src/components/numbers.ts
+++ src/components/numbers.ts
@@ -10,7 +10,7 @@ export function population(pop: number) {
 export function decimal(n: number): string {
     let i = 0;
     for (let m = 1; i < 15; ++i, m *= 10) {
-        if (m * n - Math.floor(m * n) < 0.001)
+        if (Math.abs(m * n - Math.round(m * n)) < 0.001)
             break;
     }
     return n.toFixed(i);
